8ff576717798b99d053d4076b511453caa3053fa
almar54 <almarosette54@gmail.com>
2023-11-22 14:55:26 +0200

    Add project files.

---
+++ CurrencyService/ICurrencyService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using CurrencyModel;
+using CurrencyBL;
+
+namespace CurrencyService
+{
+    [ServiceContract]
+    public interface ICurrencyService
+    {
+        [OperationContract]
+        CurrencyList GetAllCurrencies();
+
+        [OperationContract]
+        double Convert(Currency source, Currency dest, double amount);
+    }
+}
